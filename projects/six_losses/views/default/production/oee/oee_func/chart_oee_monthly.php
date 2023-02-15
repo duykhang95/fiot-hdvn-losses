74ab29276de4ced51934484dc60acbf7a78b5ca6
duykhang95 <46196554+duykhang95@users.noreply.github.com>
2023-02-15 17:05:25 +0700

    draft chart oee ok

--- projects/six_losses/views/default/production/oee/oee_func/chart_oee_monthly.php
+++ projects/six_losses/views/default/production/oee/oee_func/chart_oee_monthly.php
@@ -1,5 +1,48 @@
 <?php
-    require_once "data_chart_monthly.php";
+    // require_once "data_chart_monthly.php";
+    $select_line = isset($_GET['select_line']) ? $_GET['select_line'] : NULL;
+
+    $arr_perform = array();
+    $arr_longDt_daily = array();
+    $arr_shortDt_daily = array();
+    $arr_outputNg_daily = array();
+    $arr_speed_daily = array();
+    $arr_prepare_daily = array();
+    $arr_chaging_daily = array();
+
+    if($select_line == 'jatco'){
+        $arr_perform = [91.5, 90.05, 51.57, 73.05, 80.15, 78.89, 77.19, 87.36, 80.48, 43.82, 9.02, 39.34, 89.11, ];
+        $arr_longDt_daily = [4.27, 5.54, 45.78, 14.81, 11.46, 16.27, 14.49, 5.42, 12.75, 45.59, 85.57, 49.1, 6.72];
+        $arr_shortDt_daily = [2.26, 3.46, 2.09, 2.18, 1.6, 1.34, 3.78, 2.32, 1.47, 4.36, 3.4, 1.48, 5.55];
+        $arr_outputNg_daily = [0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01];
+        $arr_speed_daily = [12.3, 9.21, 5.6, 5.4, 8.94, 8.79, 11.86, 7.98, 4.62, 5.19, 0.68, 2.3, 9.49];
+        $arr_prepare_daily = [0, 0, 0, 1.51, 3.72, 1.75, 1.49, 0.39, 1.42, 1.27, 0, 0, 1.76];
+        $arr_chaging_daily = [0.99, 1.44, 0.69, 0.65, 0.61, 0.84, 1.22, 1.46, 1.18, 0.89, 0, 0.6, 1.4];
+        
+    }
+    else if($select_line == 'aw3'){
+        $arr_perform = [76.74, 82.73, 57.5, 0, 22.45, 69.77, 74.68, 70.68, 91.15, 84.24, 48.63, 0, 85.3,];
+        $arr_longDt_daily = [13.94, 10.5, 34.22, 0, 61.66, 25.58, 17.18, 21.13, 5.15, 12.15, 45.44, 0, 8.09];
+        $arr_shortDt_daily = [3.75, 1.98, 2.26, 0, 0.42, 5.05, 3.22, 3.48, 2.33, 2.78, 2.03, 0, 4.39];
+        $arr_outputNg_daily = [0.01, 0.01 ,0.01, 0, 0.01, 0.01, 0.01, 0.01, 0.03, 0,01, 0.01, 0, 0.01];
+        $arr_speed_daily = [14.66, 13.88, 11.04, 0, 3.71, 24.2, 19.7, 17.09, 8.71, 8, 6.05, 0, 11.32];
+        $arr_prepare_daily = [1.22, 5.54, 5.7, 0.26, 0, 8.55, 4.91, 5.14, 1.06, 5.12, 1.01, 0, 0];
+        $arr_chaging_daily = [1.93, 1.82, 1.53, 0, 0.53, 17.43, 1.88, 11.66, 2.09, 2.18, 1.68, 0, 2.03];
+    }
+    else if($select_line == 'aw2'){
+        $arr_perform = [33.74, 71.64, 56.77, 0, 0, 30.82, 59.42, 84.78, 86.11, 77.95, 75.64, 0, 82.99];
+        $arr_longDt_daily = [53.9, 21.23, 30.34, 0, 0, 60.41, 26.64, 7.04, 7.43, 18.83, 15.34, 0, 7.14];
+        $arr_shortDt_daily = [2.59, 8.26, 4.46, 0, 0, 7.01, 4.36, 6.11, 3.72, 6.95, 2.47, 0, 4.38];
+        $arr_outputNg_daily = [0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01];
+        $arr_speed_daily = [4.5, 11.52, 7.17, 0, 0, 5.75, 6.18, 9.09, 9.35, 9.92, 8.26, 0, 10.27];
+        $arr_prepare_daily = [4.96, 7.96, 1.27, 3.66, 0, 4.62, 1.84, 2.77, 5.56, 2.41, 2.57, 3.15, 5.57];
+        $arr_chaging_daily = [0.39, 1.55, 1.03, 0, 0, 0.64, 1.17, 1.7, 1.92, 2.87, 1.83, 0, 1.79];
+    }
+    
+
+
+
+
 ?>
 <div id="oee_monthly"></div>
 
@@ -66,45 +109,52 @@
         series: [
             {
             name: "Tốc độ",
-            data: [<?php for($i = 0; $i < count($arr_speed_daily); $i++){
-                echo $arr_speed_daily[$i] . ",";
+            data: [<?php 
+                for($i = 0; $i < count($arr_speed_daily); $i++){
+                    echo $arr_speed_daily[$i] . ",";
             } ?>],
             },
             {
             name: "Chuẩn bị",
-            data: [<?php for($i = 0; $i < count($arr_prepare_daily); $i++){
-                echo $arr_prepare_daily[$i] . ",";
+            data: [<?php 
+                for($i = 0; $i < count($arr_prepare_daily); $i++){
+                    echo $arr_prepare_daily[$i] . ",";
             } ?>],
             },
             {
             name: "Thay đổi",
-            data: [<?php for($i = 0; $i < count($arr_chaging_daily); $i++){
-                echo $arr_chaging_daily[$i] . ",";
+            data: [<?php 
+                for($i = 0; $i < count($arr_chaging_daily); $i++){
+                    echo $arr_chaging_daily[$i] . ",";
             } ?>],
             },
             {
             name: "Hàng NG",
-            data: [<?php for($i = 0; $i < count($arr_outputNg_daily); $i++){
-                echo $arr_outputNg_daily[$i] . ",";
+            data: [<?php 
+                for($i = 0; $i < count($arr_outputNg_daily); $i++){
+                    echo $arr_outputNg_daily[$i] . ",";
             } ?>],
             },
             {
             name: "Dừng dài",
-            data: [<?php for($i = 0; $i < count($arr_longDt_daily); $i++){
-                echo $arr_longDt_daily[$i] . ",";
+            data: [<?php 
+                for($i = 0; $i < count($arr_longDt_daily); $i++){
+                    echo $arr_longDt_daily[$i] . ",";
             } ?>],
             },
             {
             name: "Dừng ngắn",
-            data: [<?php for($i = 0; $i < count($arr_shortDt_daily); $i++){
-                echo $arr_shortDt_daily[$i] . ",";
+            data: [<?php 
+                for($i = 0; $i < count($arr_shortDt_daily); $i++){
+                    echo $arr_shortDt_daily[$i] . ",";
             } ?>],
             color: "#ff0000",
             },
             {
             name: "Hiệu suất",
-            data: [<?php for($i = 0; $i < count($arr_perform); $i++){
-                echo $arr_perform[$i] . ",";
+            data: [<?php 
+                for($i = 0; $i < count($arr_perform); $i++){
+                    echo $arr_perform[$i] . ",";
             } ?>],
             color: "#0099ff",
             },
---
+++ projects/six_losses/views/default/production/oee/oee_func/data_chart_draft.php
@@ -0,0 +1,388 @@
+<pre>
+<?php 
+    require_once "config/config.php";
+    $connect = $GLOBALS['connect'];
+    
+    $date_now = date('Y-m-d H:i:s');
+    $date = date('Y-m-d');
+    
+    $yesterday_tmp = mktime(0, 0, 0, date("m") , date("d")-1, date("Y"));
+    $yesterday = date('Y-m-d', $yesterday_tmp);
+
+    $year = date('Y');
+    $month = date('m');
+
+    $select_line = isset($_GET['select_line']) ? $_GET['select_line'] : NULL;
+    // $select_line = "aw3";
+    $count_day = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+    // $count_day = cal_days_in_month(CAL_GREGORIAN, 05, 2022);
+    $select_month = date('Y-m') . "-01";
+    // echo $select_month . "</br>";
+    if($month == '12' ){
+        $first_month_next = date('Y')+1 . "-" ."01" . "-01";
+    }
+    else{
+        $first_month_next = date('Y') . "-" .date('m')+1 . "-01";
+    }
+    // $first_month_next = date('Y') . "-" .date('m')+1 . "-01";
+    // $select_month = "2022-05";
+    // echo $select_month . "</br>";
+    // echo $count_day . "</br>";
+
+    //-----------------------Lọc dữ liệu output full tháng--------------------------------
+    $sql_output= "SELECT * FROM " . $select_line . "_output_5_2 " . "WHERE time BETWEEN '$select_month 06:00:00' AND '$first_month_next 06:00:00' ORDER BY time DESC";
+    // echo $sql_output;
+    $result_output = mysqli_query( $connect, $sql_output );
+    if ($result_output && $result_output->num_rows > 0) {
+        $i = 0;
+        while ($row = $result_output->fetch_assoc()) {
+            $data_output[$i][0]=$row['id'];
+            $data_output[$i][1]=$row['output'];
+            $data_output[$i][2]=$row['time'];
+            $i++;
+        }
+    }
+    else{
+        $data_output = [];
+    }
+
+    // print_r($data_output);
+    // time : 2022-05-11 07:42:03
+    // $arr_day =["2022-05-01","2022-05-02","2022-05-03"];
+    $arr_day = array();
+    for($i=1; $i <= $count_day; $i++){
+        if($i<10){
+            array_push($arr_day, date('Y-m').'-0'. $i);
+        }
+        else{
+            array_push($arr_day, date('Y-m').'-'. $i);
+        }
+    }
+    array_push($arr_day, date('Y') . "-" .date('m')+1 . "-01");
+    // $count = 0;
+    // print_r($arr_day) . "<br>";
+    $arr_output_day = array();
+    // print_r($arr_day);
+    for($i = 0; $i < count($arr_day)-1; $i++){
+        $output_daily = 0;
+        for($x = 0; $x < count($data_output); $x++){
+            if($i <(count($arr_day)-1) && $data_output[$x][2] >= ($arr_day[$i] . " 06:00:00") && $data_output[$x][2] < ($arr_day[$i+1] . " 06:00:00")){
+                $output_daily += $data_output[$x][1];
+            }
+        }
+        array_push($arr_output_day, $output_daily);
+    }
+    // print_r($arr_output_day);
+
+    //output target for one day
+    // $output_standard = "SELECT sum(output_target) AS output_std FROM " . $select_line . "_output_target " . "ORDER BY id ASC";
+    if($select_line == 'jatco'){
+        $output_standard = "SELECT sum(output_target) AS output_std FROM " . "aw3_output_target " . "WHERE id BETWEEN 1 AND 8";
+    }
+    else{
+        $output_standard = "SELECT sum(output_target) AS output_std FROM " . "aw3_output_target " . "WHERE id BETWEEN 9 AND 24";
+    }
+    $result_output_standard = mysqli_query($connect, $output_standard);
+    if($result_output_standard && $result_output_standard -> num_rows > 0){
+        while ($row = mysqli_fetch_array($result_output_standard)) { 
+            $data_output_standard = $row['output_std'];
+            // echo $data_output_standard;
+        }
+    }
+
+    // echo $data_output_standard . "</br>";
+    $arr_perform = array();
+    for($i = 0; $i < count($arr_output_day); $i++){
+        $perform_daily = ($arr_output_day[$i] / $data_output_standard) * 100;
+        $perform_daily = round($perform_daily, 2);
+        array_push($arr_perform, $perform_daily);
+    }
+    // echo 'Hieu suat . <br>';
+    print_r($arr_perform);
+
+    $sql_long_dt = "SELECT period, name, time FROM " . $select_line . "_long_downtime " . "WHERE time BETWEEN '$select_month 06:00:00' AND '$first_month_next 06:00:00' AND period > 300 ORDER BY time DESC";
+    // echo $sql_long_dt . "</br>";
+    $result_long_dt = mysqli_query( $connect, $sql_long_dt );
+    if ($result_long_dt && $result_long_dt->num_rows > 0) {
+        $i = 0;
+        while ($row = $result_long_dt->fetch_assoc()) {
+            $data_long_dt[$i][0]=$row['period'];
+            $data_long_dt[$i][1]=$row['name'];
+            $data_long_dt[$i][2]=$row['time'];
+            $i++;
+        }
+    }
+    else{
+        $data_long_dt = [];
+    }
+    // print_r($data_long_dt);
+    $arr_longDt_daily = array();
+    for($i = 0; $i < count($arr_day)-1; $i++){
+        $period_longDt_daily = 0;
+        for($x = 0; $x < count($data_long_dt); $x++){
+            if($data_long_dt[$x][2] >= ($arr_day[$i] . " 06:00:00") && $data_long_dt[$x][2] < ($arr_day[$i+1] . " 06:00:00")){
+                $period_longDt_daily += ($data_long_dt[$x][0] / (60*860)) * 100;
+                // echo $period_shortDt_daily . "<br>";
+                
+            }
+        }
+        $period_longDt_daily = round($period_longDt_daily, 2);
+        array_push($arr_longDt_daily, $period_longDt_daily);
+    }
+
+    // print_r($arr_longDt_daily);
+
+    $sql_short_dt = "SELECT period, name, time FROM " . $select_line . "_long_downtime " . "WHERE time BETWEEN '$select_month 06:00:00' AND '$first_month_next 06:00:00' AND period < 300 ORDER BY time DESC";
+    // echo $sql_short_dt . "</br>";
+    $result_short_dt = mysqli_query( $connect, $sql_short_dt );
+    if ($result_short_dt && $result_short_dt->num_rows > 0) {
+        $i = 0;
+        while ($row = $result_short_dt->fetch_assoc()) {
+            $data_short_dt[$i][0]=$row['period'];
+            $data_short_dt[$i][1]=$row['name'];
+            $data_short_dt[$i][2]=$row['time'];
+            $i++;
+        }
+    }
+    else{
+        $data_short_dt = [];
+    }
+    // print_r ($data_short_dt);
+
+    $arr_shortDt_daily = array();
+    for($i = 0; $i < count($arr_day)-1; $i++){
+        $period_shortDt_daily = 0;
+        for($x = 0; $x < count($data_short_dt); $x++){
+            if($data_short_dt[$x][2] >= ($arr_day[$i] . " 06:00:00") && $data_short_dt[$x][2] < ($arr_day[$i+1] . " 06:00:00")){
+                $period_shortDt_daily += ($data_short_dt[$x][0] / (60* 860)) * 100;
+                // echo $period_shortDt_daily . "<br>";
+            }
+        }
+        $period_shortDt_daily = round($period_shortDt_daily, 2);
+        array_push($arr_shortDt_daily, $period_shortDt_daily);
+    }
+    // echo 'data_short_dt . </br>';
+    // print_r($arr_shortDt_daily);
+    
+
+    $sql_output_ng = "SELECT defect_name, time FROM " . $select_line . "_output_ng_total " . "WHERE time BETWEEN '$select_month 06:00:00' AND '$first_month_next 06:00:00' ORDER BY time DESC";
+    // echo $sql_output_ng . "</br>";
+    $result_output_ng = mysqli_query( $connect, $sql_output_ng );
+    if ($result_output_ng && $result_output_ng->num_rows > 0) {
+        $i = 0;
+        while ($row = $result_output_ng->fetch_assoc()) {
+            $data_output_ng[$i][0]=$row['defect_name'];
+            $data_output_ng[$i][1]=$row['time'];
+            $i++;
+        }
+    }
+    else{
+        $data_output_ng = [];
+    }
+
+    // print_r($data_output_ng);
+
+    $arr_outputNg_daily = array();
+    for($i = 0; $i < count($arr_day)-1; $i++){
+        $period_outputNg_daily = 0;
+        for($x = 0; $x < count($data_output_ng); $x++){
+            if($data_output_ng[$x][1] >= ($arr_day[$i] . " 06:00:00") && $data_output_ng[$x][1] < ($arr_day[$i+1] . " 06:00:00")){
+                $period_outputNg_daily = (int)$data_output_ng[$x][0];
+                $period_outputNg_daily++;
+                $period_outputNg_daily = (($period_outputNg_daily * 3.8) / (60 * 860)) * 100;
+                // echo $period_outputNg_daily . "<br>";
+            }
+        }
+        $period_outputNg_daily = round($period_outputNg_daily, 2);
+        array_push($arr_outputNg_daily, $period_outputNg_daily);
+    }
+    // echo 'data_ng . </br>';
+    // print_r($arr_outputNg_daily);
+
+    if($select_line == "aw2"){
+        $sql_speed = "SELECT period, time FROM " ."aw2_summary_speed " . "WHERE op = 99 AND time BETWEEN '$select_month 06:00:00' 
+            AND '$first_month_next 06:00:00' ORDER BY time DESC";
+    }else{
+        $sql_speed = "SELECT period, time FROM " . $select_line . "_summary_speed " . "WHERE time BETWEEN '$select_month 06:00:00' 
+            AND '$first_month_next 06:00:00' ORDER BY time DESC";
+    }
+    
+    // echo $sql_speed . "</br>";
+    $result_speed = mysqli_query( $connect, $sql_speed );
+    if ($result_speed && $result_speed->num_rows > 0) {
+        $i = 0;
+        while ($row = $result_speed->fetch_assoc()) {
+            $data_speed[$i][0]=$row['period'];
+            $data_speed[$i][1]=$row['time'];
+            $i++;
+        }
+    }
+    else{
+        $data_speed = [];
+    }
+
+    $arr_speed_daily = array();
+    for($i = 0; $i < count($arr_day)-1; $i++){
+        $period_speed_daily = 0;
+        for($x = 0; $x < count($data_speed); $x++){
+            if($i <(count($arr_day)-1) && $data_speed[$x][1] >= ($arr_day[$i] . " 06:00:00") && $data_speed[$x][1] < ($arr_day[$i+1] . " 06:00:00")){
+                // $period_speed_daily += round((($data_speed[$x][0] / 60) / 1275) * 100, 2);
+                $period_speed_daily += ($data_speed[$x][0] / (60*845)) * 100;
+                // echo $data_speed[$x][1] . "<br>";
+                // echo $period_speed_daily . "<br>";
+            }
+        }
+        $period_speed_daily = round($period_speed_daily, 2);
+        array_push($arr_speed_daily, $period_speed_daily);
+    }
+    // echo 'data_speed . </br>';
+    // print_r($arr_speed_daily);
+
+    $sql_prepare = "SELECT period, time FROM " . $select_line . "_line_prepare " . "WHERE time BETWEEN '$select_month 06:00:00' 
+            AND '$first_month_next 06:00:00' ORDER BY time DESC";
+    // echo $sql_prepare . "</br>";
+    $result_prepare = mysqli_query( $connect, $sql_prepare );
+    if ($result_prepare && $result_prepare->num_rows > 0) {
+        $i = 0;
+        while ($row = $result_prepare->fetch_assoc()) {
+            $data_prepare[$i][0]=$row['period'];
+            $data_prepare[$i][1]=$row['time'];
+            $i++;
+        }
+    }
+    else{
+        $data_prepare = [];
+    }
+
+    $arr_prepare_daily = array();
+    for($i = 0; $i < count($arr_day)-1; $i++){
+        $period_prepare_daily = 0;
+        for($x = 0; $x < count($data_prepare); $x++){
+            if($data_prepare[$x][1] >= ($arr_day[$i] . " 06:00:00") && $data_prepare[$x][1] < ($arr_day[$i+1] . " 06:00:00")){
+                $period_prepare_daily += ($data_prepare[$x][0] / (60*845)) * 100;
+                // echo $period_speed_daily . "<br>";
+            }
+        }
+        $period_prepare_daily = round($period_prepare_daily, 2);
+        array_push($arr_prepare_daily, $period_prepare_daily);
+    }
+    echo 'data_prepare . </br>';
+    print_r($arr_prepare_daily);
+
+    $sql_wire_electrode = "SELECT period, time FROM " . $select_line . "_change_wire " . "WHERE time BETWEEN '$select_month 06:00:00' 
+            AND '$first_month_next 06:00:00' ORDER BY time DESC";
+    // echo $sql_wire_electrode . "</br>";
+    $result_wire_electrode = mysqli_query( $connect, $sql_wire_electrode );
+    if ($result_wire_electrode && $result_wire_electrode->num_rows > 0) {
+        $i = 0;
+        while ($row = $result_wire_electrode->fetch_assoc()) {
+            $data_wire_electrode[$i][0]=$row['period'];
+            $data_wire_electrode[$i][1]=$row['time'];
+            // $data_wire_electrode[$i][2]=$row['name'];
+            $i++;
+        }
+    }
+    else{
+        $data_wire_electrode = [];
+    }
+
+    $arr_wire_electrode_daily = array();
+    for($i = 0; $i < count($arr_day)-1; $i++){
+        $period_wire_electrode_daily = 0;
+        for($x = 0; $x < count($data_wire_electrode); $x++){
+            if($data_wire_electrode[$x][1] >= ($arr_day[$i] . " 06:00:00") && $data_wire_electrode[$x][1] < ($arr_day[$i+1] . " 06:00:00")){
+                $period_wire_electrode_daily += ($data_wire_electrode[$x][0] / (60 * 845)) * 100;
+                // echo $period_speed_daily . "<br>";
+            }
+        }
+        $period_wire_electrode_daily = round($period_wire_electrode_daily, 2);
+        array_push($arr_wire_electrode_daily, $period_wire_electrode_daily);
+    }
+
+    //data change code
+    $sql_change_code = "SELECT period, time FROM " . $select_line . "_code_change " . "WHERE time BETWEEN '$select_month 06:00:00' 
+            AND '$first_month_next 06:00:00' ORDER BY time DESC";
+    // echo $sql_change_code . "</br>";
+    $result_change_code = mysqli_query( $connect, $sql_change_code );
+    if ($result_change_code && $result_change_code->num_rows > 0) {
+        $i = 0;
+        while ($row = $result_change_code->fetch_assoc()) {
+            $data_change_code[$i][0]=$row['period'];
+            $data_change_code[$i][1]=$row['time'];
+            // $data_wire_electrode[$i][2]=$row['name'];
+            $i++;
+        }
+    }
+    else{
+        $data_change_code = [];
+    }
+
+    $arr_change_code_daily = array();
+    for($i = 0; $i < count($arr_day)-1; $i++){
+        $period_change_code_daily = 0;
+        for($x = 0; $x < count($data_change_code); $x++){
+            if($data_change_code[$x][1] >= ($arr_day[$i] . " 06:00:00") && $data_change_code[$x][1] < ($arr_day[$i+1] . " 06:00:00")){
+                $period_change_code_daily += ($data_change_code[$x][0] / (60 * 845)) * 100;
+                // echo $period_speed_daily . "<br>";
+            }
+        }
+        $period_change_code_daily = round($period_change_code_daily, 2);
+        array_push($arr_change_code_daily, $period_change_code_daily);
+    }
+    echo 'data_change . </br>';
+    // print_r($arr_change_code_daily);
+    
+    //sum 2 array $arr_wire_electrode_daily and $arr_change_code_daily
+    $arr_chaging_daily = array_map(function (...$arrays) {
+        return array_sum($arrays);
+    }, $arr_change_code_daily, $arr_wire_electrode_daily);
+    print_r($arr_chaging_daily);
+
+    //hieu suat den ngay 13/02/2023
+    // [0] => 91.5
+    // [1] => 90.05
+    // [2] => 51.57
+    // [3] => 73.05
+    // [4] => 80.15
+    // [5] => 78.89
+    // [6] => 77.19
+    // [7] => 87.36
+    // [8] => 80.48
+    // [9] => 43.82
+    // [10] => 9.02
+    // [11] => 39.34
+    // [12] => 109.11
+    // [13] => 3.71
+    $arr_perform_jatco = [91.5, 90.05, 51.57, 73.05, 80.15, 78.89, 77.19, 87.36, 80.48, 43.82, 9.02, 39.34, 89.11, ];
+
+    // [0] => 76.74
+    // [1] => 82.73
+    // [2] => 57.5
+    // [3] => 0.63
+    // [4] => 22.45
+    // [5] => 69.77
+    // [6] => 74.68
+    // [7] => 70.68
+    // [8] => 91.15
+    // [9] => 84.24
+    // [10] => 48.63
+    // [11] => 0
+    // [12] => 85.3
+    $arr_perform_aw3 = [76.74, 82.73, 57.5, 0, 22.45, 69.77, 74.68, 70.68, 91.15, 84.24, 48.63, 0, 85.3];
+    
+    // [0] => 33.74
+    // [1] => 71.64
+    // [2] => 56.77
+    // [3] => 0
+    // [4] => 0
+    // [5] => 30.82
+    // [6] => 59.42
+    // [7] => 84.78
+    // [8] => 86.11
+    // [9] => 77.95
+    // [10] => 75.64
+    // [11] => 0[33.74, 71.64, 56.77, 0, 0, 30.82, 59.42, 84.78, 86.11, 77.95, 75.64, 0, 82.99];
+    // [12] => 82.99
+    $arr_perform_aw2 = [33.74, 71.64, 56.77, 0, 0, 30.82, 59.42, 84.78, 86.11, 77.95, 75.64, 0, 82.99];
+
+?>
